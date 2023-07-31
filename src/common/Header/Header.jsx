2e0c290b7343edbe6444b2ada9d2f7b85e0fed91
OchandoDevSergio <sergio.ochando.dev@gmail.com>
2023-07-31 18:59:19 +0200

    navigate buttons added

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,8 +1,17 @@
+
+import { useNavigate } from 'react-router-dom';
 import './Header.css'
 
 export const Header = () => {
 
+    //Instanciamos useNavigate dentro de navigate para poder navegar
+    const navigate = useNavigate();
+
     return (
-        <div className='header'>soy Header</div>
+        <div className='header'>
+            <div className="buttonClicker" onClick={()=>navigate("/")}>Home</div>
+            <div className="buttonClicker" onClick={()=>navigate("/login")}>Login</div>
+            <div className="buttonClicker" onClick={()=>navigate("/register")}>Register</div>
+        </div>
     )
 }
