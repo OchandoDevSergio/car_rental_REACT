681e0428cfb8f94a6b11517df43c4680203e1b34
OchandoDevSergio <sergio.ochando.dev@gmail.com>
2023-08-27 13:58:20 +0200

    starting to make the showcar page

--- src/pages/Body/Body.jsx
+++ src/pages/Body/Body.jsx
@@ -4,7 +4,7 @@ import { Login } from "../Login/Login";
 import { Register } from "../Register/Register";
 import {NewCar } from "../NewCar/NewCar";
 import { Profile } from "../Profile/Profile";
-//import { ShowCar } from "../ShowCar/ShowCar";
+import { ShowCar } from "../ShowCar/ShowCar";
 
 export const Body = () => {
   return (
@@ -15,7 +15,7 @@ export const Body = () => {
         <Route path="/register" element={<Register />} />
         <Route path="/newcar" element={<NewCar />} />
         <Route path="/profile" element={<Profile />} />
-        {/* <Route path="/showcar" element={<ShowCar />} /> */}
+        <Route path="/showcar" element={<ShowCar />} />
       </Routes>
     </>
   );
--- src/pages/ShowCar/ShowCar.jsx
+++ src/pages/ShowCar/ShowCar.jsx
@@ -1,1 +1,21 @@
 import './ShowCar.css'
+
+
+export const ShowCar = () => {
+
+
+
+return (
+    <div className="container-fluid showCar">
+       <img src={car.picture} alt={car.id}/>
+       <div>{car.model}</div>
+       <div>{car.year}</div>
+       <div>{car.plate}</div>
+        
+    </div>
+)
+
+
+
+
+}
