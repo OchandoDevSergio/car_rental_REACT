cb3fab832c891a9b67ce97875cc63ff9c858cc3f
OchandoDevSergio <sergio.ochando.dev@gmail.com>
2023-09-11 23:05:56 +0200

    added a research for cars by a model input

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -3,7 +3,9 @@ import "./Header.css";
 
 import { useSelector, useDispatch } from "react-redux";
 import { userDataCheck, userout } from "../../pages/userSlice";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Input } from "../Input/Input";
+import { searchCriteria } from "../../services/apiCalls";
 export const Header = () => {
   //Instanciamos useNavigate dentro de navigate para poder navegar
   const navigate = useNavigate();
@@ -14,6 +16,8 @@ export const Header = () => {
   //Instanciamos Redux en modo ESCRITURA
   const dispatch = useDispatch();
 
+  const [criteria, setCriteria] = useState("");
+
   // useEffect(()=>{
   //     console.log("soy REDUX con el HEADER .... y cambio a.....", reduxUserData.credentials?.userData)
   // },[reduxUserData]);
@@ -23,28 +27,50 @@ export const Header = () => {
     navigate("/");
   };
 
+  const inputHandler = (e) => {
+    setCriteria(e.target.value);
+  };
+
+  useEffect(() => {
+    if(criteria !== ""){
+      searchCriteria(criteria)
+        .then(
+          results => {
+            console.log(results)
+          }
+        )
+        .catch(error => console.log(error));
+    }
+  }, [criteria]);
+
   return (
     <div className="header">
+      <img
+        className="homeButton"
+        src="https://i.ibb.co/4VrXmfC/logoCR.jpg"
+        alt="logo"
+        onClick={() => navigate("/")}
+      />
+      <div className="spaceDiv"></div>
+      <Input
+        type={"text"}
+        placeholder=""
+        value={criteria}
+        name={"criteria"}
+        className="defaultInput"
+        manejadora={inputHandler}
+      />
       {reduxUserData.credentials.token ? (
         <>
-          <img
-            className="homeButton"
-            src="https://i.ibb.co/4VrXmfC/logoCR.jpg"
-            alt="logo"
-            onClick={() => navigate("/")}
-          />
-          <div className="spaceDiv"></div>
           <div className="loginRegisterDiv">
-            {
-              reduxUserData.credentials.userData.roleId === 1 &&
+            {reduxUserData.credentials.userData.roleId === 1 && (
               //Este AND haría referencia a un entonces.....
 
               <div className="buttonClicker" onClick={() => navigate("/admin")}>
                 Admin
               </div>
+            )}
 
-            }
-            
             <div className="buttonClicker" onClick={() => navigate("/profile")}>
               {reduxUserData.credentials?.userData?.userName}
             </div>
@@ -55,13 +81,6 @@ export const Header = () => {
         </>
       ) : (
         <>
-          <img
-            className="homeButton"
-            src="https://i.ibb.co/4VrXmfC/logoCR.jpg"
-            alt="logo"
-            onClick={() => navigate("/")}
-          />
-          <div className="spaceDiv"></div>
           <div className="loginRegisterDiv">
             <div className="buttonClicker" onClick={() => navigate("/login")}>
               Login
--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -1,20 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const bringCars = async () => {
-
-    return await axios.get(`http://localhost:5000/cars`);
-}
+  return await axios.get(`http://localhost:5000/cars`);
+};
 
 export const bringUsers = async (userData) => {
-    console.log(userData);
-    //console.log("a ver que trae el userdata.token", userData.token);
+  console.log(userData);
+  //console.log("a ver que trae el userdata.token", userData.token);
 
-    return await axios.get(`http://localhost:5000/users`, {
-        headers: {
-          authorization: "Bearer " + userData.token,
-        }
-    });
-}
+  return await axios.get(`http://localhost:5000/users`, {
+    headers: {
+      authorization: "Bearer " + userData.token,
+    },
+  });
+};
 
 // export const bringUsers = async (credentials) => {
 
@@ -22,31 +21,30 @@ export const bringUsers = async (userData) => {
 // }
 
 export const loginUser = async (credentials) => {
-
-    //aqui es donde enviamos al backend los datos de logueo desde el frontend
-    return await axios.post(`http://localhost:5000/auth/login`, credentials)
-}
+  //aqui es donde enviamos al backend los datos de logueo desde el frontend
+  return await axios.post(`http://localhost:5000/auth/login`, credentials);
+};
 
 export const registerUser = async (registerBody) => {
-
-    return axios.post(`http://localhost:5000/users`, registerBody)
-}
+  return axios.post(`http://localhost:5000/users`, registerBody);
+};
 
 export const registerCar = async (newCarBody, userData) => {
-
-    return axios.post(`http://localhost:5000/cars`, newCarBody, {
-        headers: {
-          authorization: "Bearer " + userData.token,
-        }
-})
-}
+  return axios.post(`http://localhost:5000/cars`, newCarBody, {
+    headers: {
+      authorization: "Bearer " + userData.token,
+    },
+  });
+};
 
 export const modifyUser = async (modifyUserBody, userData) => {
+  return axios.put(`http://localhost:5000/users`, modifyUserBody, {
+    headers: {
+      authorization: "Bearer " + userData.token,
+    },
+  });
+};
 
-    return axios.put(`http://localhost:5000/users`, modifyUserBody, {
-        headers: {
-          authorization: "Bearer " + userData.token,
-        }
-    })
- 
-}
+export const searchCriteria = async (criteria) => {
+  return await axios.get(`http://localhost:5000/cars/${criteria}`);
+};
